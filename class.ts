7e79f9f1715a9d6d8e8c27bac1295a2a6112207e
Kumail786 <kumailrawjani900@gmail.com>
2019-12-07 00:44:25 +0500

    class Inheritance concept

--- class.ts
+++ class.ts
@@ -2,7 +2,7 @@ class User {
   name: string;
   email: string;
   age: number;
-//if we put private before properties it will not be able to access from outside the class
+  //if we put private before properties it will not be able to access from outside the class
   constructor(name: string, email: string, age: number) {
     this.name = name;
     this.email = email;
@@ -10,8 +10,29 @@ class User {
 
     console.log("User Created " + this.name);
   }
+  register() {
+    console.log(this.name + " Registered");
+  }
 }
 
-var kumail = new User("Kumail", "kum@gmail.com", 20);
 //now we have access to all the properties of class User in kumail
-console.log(kumail.age)
+// console.log(kumail.age)
+
+/*Concept of Inheritance 
+
+If we inherit class from another class we have all the properties of the super like inthis case we write
+Member extends User so Member class has access to all public/protected properties of class User.
+First we call constructor and then we call super in which we write what properties we are going to use from super class
+*/
+class Member extends User {
+  id: number;
+  constructor(id: number, name: string, email: string, age: number) {
+    super(name, age, email);
+    this.id = id;
+  }
+  register() {
+    super.register();
+  }
+}
+var kumail = new User("Kumail", "kum@gmail.com", 20);
+kumail.register();
